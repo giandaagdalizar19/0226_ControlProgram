52ee05ad62377e5771f628864546ce12510a6189
giandaagdalizar19 <giandaagdalizar19@gmail.com>
2026-03-09 20:16:41 +0700

    selesai

--- file1.cpp
+++ file1.cpp
@@ -26,5 +26,9 @@ int main(){
     //    status = "Lolos";
     //else
     //    status = "Gagal";
+    rerata = Rata_rata(nilBI,nilMT);
+    //status = Status_lulus(rerata);
+
+    cout << "Status kelulusan ="<<Status_lulus(rerata)<<", dengan nilai rerata = "<<rerata<<endl;
 
 }
